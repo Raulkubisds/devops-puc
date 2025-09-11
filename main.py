ac956ba7e9c7c6a8c1f6154b549666a19b745446
Raulkubisds <147261111+Raulkubisds@users.noreply.github.com>
2025-09-11 15:30:05 -0300

    Ajustando CI-CD 01

--- main.py
+++ main.py
@@ -6,6 +6,5 @@ def test_read_root():
     response = read_root()
     assert response == {"hello": "world"}
 
-
 def read_root():
     return {"hello": "world"}
--- test_primeiro.py
+++
@@ -1,8 +0,0 @@
-from main import read_root
-
-def test_passando():
-    assert True
-
-def test_read_root():
-    response = read_root()
-    assert response == {"hello": "world"}
