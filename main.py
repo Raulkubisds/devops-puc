9b9c81927be63169895db801049c950e47372d93
Raulkubisds <147261111+Raulkubisds@users.noreply.github.com>
2025-09-14 11:05:36 -0300

    Tentando usar o Actions
    repliquei o trabalho, pois o github actions estava falhando

---
+++ main.py
@@ -0,0 +1,9 @@
+def test_passando():
+    assert True
+
+def test_read_root():
+    response = read_root()
+    assert response == {"hello": "world"}
+
+def read_root():
+    return {"hello": "world"}
