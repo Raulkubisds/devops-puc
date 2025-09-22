3a4584760e45c6188f618ed8b49fcf869bfdfc4d
Raulkubisds <147261111+Raulkubisds@users.noreply.github.com>
2025-09-22 14:56:53 -0300

    enviar  mensagem para o Discord

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"mensagem": "API funcionando no Docker!"}
+
+
+def read_root():
+    return {"mensagem": "API funcionando!"}
--- tests/test_01.py
+++ tests/test_01.py
@@ -11,3 +11,36 @@ def test_read_root():
     """Teste da função read_root()."""
     response = read_root()
     assert response == {"hello": "world"}
+
+# tests/test_app.py
+
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+def test_root_message():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"mensagem": "API funcionando no Docker!"}
+
+def test_soma_positiva():
+    response = client.get("/soma/2/3")
+    assert response.status_code == 200
+    assert "resultado" in response.json()
+    assert response.json() == {"resultado": 5}
+
+def test_soma_zero():
+    response = client.get("/soma/0/5")
+    assert response.status_code == 200
+    assert response.json()["resultado"] == 5
+
+def test_soma_negativa():
+    response = client.get("/soma/-2/-3")
+    assert response.status_code == 200
+    assert response.json()["resultado"] == -5
+
+def test_soma_valores_mistos():
+    response = client.get("/soma/-2/3")
+    assert response.status_code == 200
+    assert response.json()["resultado"] == 1
